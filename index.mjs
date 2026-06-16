b1760c2cfce8db3ae96a1d60d1c34b74cc129717
andresaccastello <andres.soltec2@gmail.com>
2026-06-16 11:14:54 -0300

    ,

--- index.mjs
+++ index.mjs
@@ -1,25 +1,33 @@
-import exress from 'express'
-/*import 'dotenv/config'*/
+import express from 'express'
+import 'dotenv/config'
 import pg from 'pg'
-const PUERTO =  process.env.PORT || 3000
 
-const app = exress()
-/*CONSULTA */
+const PUERTO = process.env.PORT || 3000
+
+const app = express()
+
 const pool = new pg.Pool({
-    host:process.env.BD_HOST,
+    host: process.env.BD_HOST,
     user: process.env.BD_USER,
-    database : process.env.BD_NAME,
-    pass: process.env.BD_PASS,
+    database: process.env.BD_NAME,
+    password: process.env.BD_PASS,
     port: process.env.BD_PORT
 })
 
-app.get('/test', (req, res)=>{
+app.get('/test', (req, res) => {
     res.send('hola')
 })
 
-app.get('/prueba', async (req, res)=>{
-    const resultado = await pool.query('SELECT * FROM mensajes') 
-    res.json(resultado.rows)
+app.get('/prueba', async (req, res) => {
+    try {
+        const resultado = await pool.query('SELECT * FROM mensajes')
+        res.json(resultado.rows)
+    } catch (error) {
+        console.log(error)
+        res.sendStatus(500)
+    }
 })
 
-app.listen(PUERTO)
+app.listen(PUERTO, () => {
+    console.log(`Servidor funcionando en puerto ${PUERTO}`)
+})
